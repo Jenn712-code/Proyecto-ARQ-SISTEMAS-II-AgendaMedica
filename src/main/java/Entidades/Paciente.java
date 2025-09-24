9c7d4d7cae4c5e0a11310452111541a9eccca2cd
JenniferLV285 <jlopezv6@ucentral.edu.co>
2025-09-24 11:32:46 -0500

    Creacion de entidad paciente y carpetas (recursos, repositorios, servicios)

--- src/main/java/Entidades/Paciente.java
+++ src/main/java/Entidades/Paciente.java
@@ -1,2 +1,31 @@
-package Entidades;public class Paciente {
+package Entidades;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+@Table (name = "Pacientes")
+public class Paciente extends PanacheEntity {
+
+    private Integer pacId;
+    private String pacNombre;
+    private Date pacFecNacimiento;
+    private String pacEPS;
+    private Integer pacCelular;
+    private String pacCorreo;
+    private String pacContraseña;
 }
+
+//@ManyToOne(targetEntity = Paciente.class)
+//@JoinColumn(name = "pacId")
+//@ToString.Exclude
+//private Paciente paciente;
