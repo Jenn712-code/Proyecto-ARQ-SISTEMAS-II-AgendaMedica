6d61d09da5c383652746d1b5376660806bdafd49
JenniferLV285 <jlopezv6@ucentral.edu.co>
2025-09-26 18:35:58 -0500

    Se añade entidad medicamento con dto, recurso, servicio y repositorio para la HU registrar medicamento

---
+++ src/main/java/entidades/Medicamento.java
@@ -0,0 +1,46 @@
+package entidades;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+@Table (name = "Medicamentos")
+public class Medicamento extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "medId", length = 50, nullable = false, unique = true)
+    private Integer medId;
+
+    @Column(name = "medNombre", length = 100, nullable = false)
+    private String medNombre;
+
+    @Column(name = "medDosis", length = 50, nullable = false)
+    private String medDosis;
+
+    @Column(name = "medFrecuencia", length = 50, nullable = false)
+    private Integer medFrecuencia;
+
+    @Column(name = "medDuracion", length = 50, nullable = false)
+    private Integer medDuracion;
+
+    @Column(name = "medEstado", length = 50, nullable = false)
+    private String medEstado;
+
+    @Column(name = "medRecordatorio", nullable = false)
+    private Boolean medRecordatorio;
+
+    @ManyToOne
+    @JoinColumn(name = "pacCedula")
+    private Paciente paciente;
+}
+
+
+
+
