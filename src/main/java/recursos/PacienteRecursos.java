e53025ba2225d2b5c1eaa90915d7e054d545f7eb
JenniferLV285 <jlopezv6@ucentral.edu.co>
2025-11-23 10:49:34 -0500

    HU014 Registro de nuevo usuario finalizada, corrección del servicio recordatorio para la configuración del mismo para pacientes nuevos

---
+++ src/main/java/recursos/PacienteRecursos.java
@@ -0,0 +1,29 @@
+package recursos;
+
+import dto.PacienteDTO;
+import entidades.Paciente;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+import servicios.PacienteServicios;
+
+@AllArgsConstructor
+@Path("/pacientes")
+public class PacienteRecursos {
+
+
+    PacienteServicios pacienteServicios;
+
+    @POST
+    @Path("/crear")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crear(PacienteDTO dto) {
+        Paciente paciente = pacienteServicios.crearPaciente(dto);
+        return Response.status(Response.Status.CREATED).entity(paciente).build();
+    }
+}
